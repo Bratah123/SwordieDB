1269f02590250151fa71f595e6e3b2dff6eb8db8
Brandon <brandonnguyen3301@gmail.com>
2020-11-10 12:40:37 -0800

    initialized pip setup files

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='swordiedb',
+    version='0.0.1',
+    description='A database API for swordie-ms based maplestory sources',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Brandon Nguyen',
+    author_email='brandonnguyen3301@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='database, swordie',
+    packages=find_packages(),
+    install_requires=['mysql-connector-python-rf']
+)
