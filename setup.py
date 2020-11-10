6894f36c80c9fb2a04fd681a7357d1506a35ec07
Brandon <brandonnguyen3301@gmail.com>
2020-11-10 14:03:02 -0800

    possible fix to get_job_name()

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
     name='swordiedb',
-    version='0.0.3',
+    version='0.0.4',
     description='A database API for swordie-ms based maplestory sources',
     long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='',
